958a38b3a99a150b4828c3edd3c9780cbf5923ec
maryia.siankova <toemis13@gmail.com>
2020-10-01 21:58:51 +0300

    Added test to amend first contact

--- fixture/contact.py
+++ fixture/contact.py
@@ -97,3 +97,21 @@ class ContactHelper:
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
 
+    def amend_first_contact(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        # submit changes
+        wd.find_element_by_name("update").click()
+        self.app.return_home_page()
+
+
+
---
+++ test/test_amend_contact.py
@@ -0,0 +1,14 @@
+from model.contact import Contact
+
+
+def test_amend_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.amend_first_contact(
+        Contact(first_name="Name000", middle_name="Middle000", last_name="Surname000", nickname="Toemis",
+                title="qwweqw", company="12edsdfe", address="1230009 gfhfj Rjfdkmdv; fff",
+                home="123123123", mobile="2323232323", work_phone="45455454544", fax="67676767",
+                email="toemis13@gmil.com", email2="qweqwe@test.ry", email3="asdasd@rty.by",
+                homepage="111231123.com", birth_day="5", birth_month="October",
+                birth_year="1989", aniv_day="15", aniv_month="July", aniv_year="2009",
+                address2="123123 fddfg trtrtg 555", phone2="24/3", notes="fgdg grrtthg tr"))
+    app.session.logout()
