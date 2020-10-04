d69e172658be33dc769fb59cc3112098435ab6a6
maryia.siankova <toemis13@gmail.com>
2020-10-04 11:04:53 +0300

    Added possibility to modify only necessary fields for group

--- test/test_amend_group.py
+++ test/test_amend_group.py
@@ -2,7 +2,19 @@
 from model.group import Group
 
 
-def test_amend_first_group(app):
+def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.amend_first_group(Group(name="amended", header="group second", footer="my group amended"))
+    app.group.modify_first_group(Group(name="amended"))
+    app.session.logout()
+
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="group second"))
+    app.session.logout()
+
+
+def test_modify_group_footer(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(footer="my group amended"))
     app.session.logout()
