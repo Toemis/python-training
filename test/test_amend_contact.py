92290c620c1eb113bb1c334e0ddbabd569cbc34a
maryia.siankova <toemis13@gmail.com>
2020-10-15 22:36:56 +0300

    Added comparing of contact lists after amending of element

--- test/test_amend_contact.py
+++ test/test_amend_contact.py
@@ -5,10 +5,24 @@ from model.contact import Contact
 def test_amend_contact_first_name(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="New", last_name="New"))
-    app.contact.amend_first_contact(Contact(first_name="Name000"))
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(first_name="Name000")
+    contact.id = old_contacts[0].id
+    app.contact.amend_first_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_amend_contact_last_name(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="New", last_name="New"))
-    app.contact.amend_first_contact(Contact(last_name="Surname000"))
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(last_name="Surname000")
+    contact.id = old_contacts[0].id
+    app.contact.amend_first_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
