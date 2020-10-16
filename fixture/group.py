d09511378bd2d8278e657893abadd7bcfbfc9bde
maryia.siankova <toemis13@gmail.com>
2020-10-16 23:18:10 +0300

    Added more random into amending and deleting for groups

--- fixture/group.py
+++ fixture/group.py
@@ -22,9 +22,12 @@ class GroupHelper:
         self.group_cache = None
 
     def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
@@ -43,10 +46,13 @@ class GroupHelper:
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(text)
 
-    def modify_first_group(self, new_group_data):
+    def modify_first_group(self):
+        self.modify_group_by_index(0)
+
+    def modify_group_by_index(self, new_group_data, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         wd.find_element_by_name("edit").click()
         self.fill_group_form(new_group_data)
         # submit changes
@@ -55,8 +61,11 @@ class GroupHelper:
         self.group_cache = None
 
     def select_first_group(self):
+        self.select_group_by_index(0)
+
+    def select_group_by_index(self, index):
         wd = self.app.wd
-        wd.find_element_by_name("selected[]").click()
+        wd.find_elements_by_name("selected[]")[index].click()
 
     def open_groups_page(self):
         wd = self.app.wd
--- test/test_amend_group.py
+++ test/test_amend_group.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+from random import randrange
 
 
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name='Created'))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     group = Group(name="amended")
-    group.id = old_groups[0].id
-    app.group.modify_first_group(group)
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(group, index)
     assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
@@ -19,12 +21,13 @@ def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(header='Created'))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     group = Group(header="group second")
-    group.id = old_groups[0].id
-    app.group.modify_first_group(group)
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(group, index)
     assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
@@ -32,10 +35,11 @@ def test_modify_group_footer(app):
     if app.group.count() == 0:
         app.group.create(Group(footer='Created'))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     group = Group(footer="my group amended")
-    group.id = old_groups[0].id
-    app.group.modify_first_group(group)
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(group, index)
     assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
