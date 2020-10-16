e5d98a17f97e8c0e76f18e185c0e193241df94b2
maryia.siankova <toemis13@gmail.com>
2020-10-16 23:39:59 +0300

    Added more random into amending and deleting for contacts

--- fixture/contact.py
+++ fixture/contact.py
@@ -60,9 +60,12 @@ class ContactHelper:
             wd.find_element_by_name(field_name).send_keys(text)
 
     def delete_first_contact(self):
+        self.delete_contact_by_index(0)
+
+    def delete_contact_by_index(self, index):
         wd = self.app.wd
         self.app.open_home_page()
-        self.select_first_contact()
+        self.select_contact_by_index(index)
         # submit deletion
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
@@ -71,13 +74,19 @@ class ContactHelper:
         self.contact_cache = None
 
     def select_first_contact(self):
-        wd = self.app.wd
-        wd.find_element_by_name("selected[]").click()
+        self.select_contact_by_index(0)
 
-    def amend_first_contact(self, contact):
+    def select_contact_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
+    def amend_first_contact(self):
+        self.amend_contact_by_index(0)
+
+    def amend_contact_by_index(self, contact, index):
         wd = self.app.wd
         self.app.open_home_page()
-        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        wd.find_elements_by_xpath("//img[@alt='Edit']")[index].click()
         # fill contact form
         self.fill_contact_form(contact)
         # submit changes
--- test/test_amend_contact.py
+++ test/test_amend_contact.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+from random import randrange
 
 
 def test_amend_contact_first_name(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="New", last_name="New"))
     old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
     contact = Contact(first_name="Name000")
-    contact.id = old_contacts[0].id
-    app.contact.amend_first_contact(contact)
+    contact.id = old_contacts[index].id
+    app.contact.amend_contact_by_index(contact, index)
     assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    old_contacts[0] = contact
+    old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
@@ -19,10 +21,11 @@ def test_amend_contact_last_name(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="New", last_name="New"))
     old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
     contact = Contact(last_name="Surname000")
-    contact.id = old_contacts[0].id
-    app.contact.amend_first_contact(contact)
+    contact.id = old_contacts[index].id
+    app.contact.amend_contact_by_index(contact, index)
     assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    old_contacts[0] = contact
+    old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
