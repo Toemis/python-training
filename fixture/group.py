0d0777814e6446977d670519793a2c76c04206f1
maryia.siankova <toemis13@gmail.com>
2020-10-16 22:46:03 +0300

    Implemented caching and hashing for groups

--- fixture/group.py
+++ fixture/group.py
@@ -19,6 +19,7 @@ class GroupHelper:
         # submit group creation form
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def delete_first_group(self):
         wd = self.app.wd
@@ -27,6 +28,7 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def fill_group_form(self, group):
         wd = self.app.wd
@@ -50,6 +52,7 @@ class GroupHelper:
         # submit changes
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def select_first_group(self):
         wd = self.app.wd
@@ -69,14 +72,17 @@ class GroupHelper:
         wd = self.app.wd
         return wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0
 
+    group_cache = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        group_list = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            group_list.append(Group(name=text, id=id))
-        return group_list
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_groups_page()
+            self.group_cache = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
 
 
