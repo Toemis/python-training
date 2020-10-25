2ec3d2b20abc3eaa130c4a2e760e3524b2838c77
maryia.siankova <toemis13@gmail.com>
2020-10-25 21:45:52 +0300

    Amended test_modify_group to use data driven approach

--- test/test_amend_group.py
+++ test/test_amend_group.py
@@ -1,42 +1,30 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 from random import randrange
+import pytest
+import random
+import string
 
 
-def test_modify_group_name(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10  # failed if add string.punctuation
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [
+    Group(name=name, header=header, footer=footer)
+    for name in ["", random_string("name", 10)]
+    for header in ["", random_string("header", 20)]
+    for footer in ["", random_string("footer", 20)]
+]
+
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_modify_group(app, group):
     if app.group.count() == 0:
         app.group.create(Group(name='Created'))
     old_groups = app.group.get_group_list()
     index = randrange(len(old_groups))
-    group = Group(name="amended")
-    group.id = old_groups[index].id
-    app.group.modify_group_by_index(group, index)
-    assert len(old_groups) == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index] = group
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-def test_modify_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(header='Created'))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    group = Group(header="group second")
-    group.id = old_groups[index].id
-    app.group.modify_group_by_index(group, index)
-    assert len(old_groups) == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index] = group
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-def test_modify_group_footer(app):
-    if app.group.count() == 0:
-        app.group.create(Group(footer='Created'))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    group = Group(footer="my group amended")
     group.id = old_groups[index].id
     app.group.modify_group_by_index(group, index)
     assert len(old_groups) == app.group.count()
