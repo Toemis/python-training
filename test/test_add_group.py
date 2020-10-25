e11df923e95bff8ab8c622cf939b32ea7227ef15
maryia.siankova <toemis13@gmail.com>
2020-10-25 15:21:55 +0300

    Amended test_add_group to use data driven approach

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+
+testdata = [
+    Group(name="new1", header="group first", footer="my group"),
+    Group(name="", header="", footer="")
+]
 
 
-def test_add_group(app):
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="new1", header="group first", footer="my group")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
@@ -12,11 +18,11 @@ def test_add_group(app):
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+# def test_add_empty_group(app):
+#     old_groups = app.group.get_group_list()
+#     group = Group(name="", header="", footer="")
+#     app.group.create(group)
+#     assert len(old_groups) + 1 == app.group.count()
+#     new_groups = app.group.get_group_list()
+#     old_groups.append(group)
+#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
