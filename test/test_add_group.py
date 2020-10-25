4a4fc32f6cbc545123c96214f3dce3128a5aab56
maryia.siankova <toemis13@gmail.com>
2020-10-25 16:14:38 +0300

    Added more random to test_add_group data driven approach

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
+import random
+import string
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10  # failed if add string.punctuation
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
 
 testdata = [
-    Group(name="new1", header="group first", footer="my group"),
-    Group(name="", header="", footer="")
+    Group(name=name, header=header, footer=footer)
+    for name in ["", random_string("name", 10)]
+    for header in ["", random_string("header", 20)]
+    for footer in ["", random_string("footer", 20)]
 ]
 
 
