6d9abedf9a1b528346b4bcbc1765842d40a90784
maryia.siankova <toemis13@gmail.com>
2020-10-28 13:40:33 +0300

    Addded base_url as parameter for command line execution

--- fixture/application.py
+++ fixture/application.py
@@ -5,7 +5,7 @@ from fixture.contact import ContactHelper
 
 
 class Application:
-    def __init__(self, browser='firefox'):
+    def __init__(self, browser, base_url):
         if browser == 'firefox':
             self.wd = webdriver.Firefox()
         elif browser == 'chrome':
@@ -14,10 +14,11 @@ class Application:
             self.wd = webdriver.Ie()
         else:
             raise ValueError("Unrecognized browser: %s" % browser)
-        self.wd.implicitly_wait(5)
+        self.wd.implicitly_wait(2)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -34,7 +35,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not self.is_home_page():
-            wd.get("http://localhost:801/addressbook/")
+            wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
