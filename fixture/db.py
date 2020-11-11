8534a5272ac6aed4672ce2ebc5ff9f17805532d5
maryia.siankova <toemis13@gmail.com>
2020-11-11 20:45:11 +0300

    Added test_home_page_vs_db_contact_info

--- fixture/db.py
+++ fixture/db.py
@@ -28,10 +28,15 @@ class DbFixture:
         contact_list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname from addressbook where deprecated = '0000-00-00 00:00:00'")
+            cursor.execute("select id, firstname, lastname, address, home, mobile, work, phone2, "
+                           "email, email2, email3 from addressbook where deprecated = '0000-00-00 00:00:00'")
             for row in cursor:
-                (id, first_name, last_name) = row
-                contact_list.append(Contact(id=str(id), first_name=first_name, last_name=last_name))
+                (id, first_name, last_name, address, home, mobile, work, phone2, email, email2, email3) = row
+                contact_list.append(Contact(id=str(id), first_name=first_name, last_name=last_name, address=address,
+                                            home_phone=home, mobile_phone=mobile, work_phone=work, phone2=phone2,
+                                            email=email, email2=email2, email3=email3, all_phones_merged=None,
+                                            all_emails_merged=None))
+
         finally:
             cursor.close()
         return contact_list
--- test/test_contact_info.py
+++ test/test_contact_info.py
@@ -1,18 +1,17 @@
 import re
 from model.contact import Contact
-from random import randrange
 
 
-def test_contact_info_on_home_page(app, db):
+def test_home_page_vs_db_contact_info(app, db):
     home_contacts_list = app.contact.get_contact_list()
     db_contact_list = db.get_contact_list()
     db_contacts_clean = map(clean, db_contact_list)
     assert sorted(home_contacts_list, key=Contact.id_or_max) == sorted(db_contacts_clean, key=Contact.id_or_max)
+    # print("db_contacts_clean:")
+    # for i in list(map(clean, db_contact_list)):
+    #     print(i)
 
-    #assert contact_from_home_page.address == clear_spaces_before_newline(contact_from_edit_page.address)
-    # assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    # assert contact_from_home_page.all_emails_from_home_page == \
-    #        clear_spaces_before_newline(merge_emails_like_on_home_page(contact_from_edit_page))
+
 
 # def test_contact_info_on_home_page(app):
 #     home_contacts_list = app.contact.get_contact_list()
@@ -31,7 +30,7 @@ def test_contact_info_on_home_page(app, db):
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.all_phones_merged == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
 def test_phones_on_contact_view_page(app):
@@ -54,17 +53,23 @@ def clear_spaces_before_newline(s):  # maybe it's not really clear and beautiful
 
 
 def merge_phones_like_on_home_page(contact):  # 5 - merge with '\n' between elements (using join function)
-    return "\n".join(filter(lambda x: x !="",  # 4 - filter empty strings that may be created after 'clear' function
+    return "\n".join(filter(lambda x: x != "",  # 4 - filter empty strings that may be created after 'clear' function
                             map(lambda x: clear(x),  # 3 - clear values from additional chars (like - or ())
                                 filter(lambda x: x is not None,  # 2 - filter None values from the list below
-                                       [contact.home_phone,  contact.mobile_phone,  # 1 - create a list of phones
+                                       [contact.home_phone, contact.mobile_phone,  # 1 - create a list of phones
                                         contact.work_phone, contact.phone2]))))
 
 
 def merge_emails_like_on_home_page(contact):  # create a list of emails with '\n' between them if email not None
-    return "\n".join(filter(lambda x: x is not None, [contact.email,  contact.email2, contact.email3]))
+    return "\n".join(filter(lambda x: x is not None, [contact.email, contact.email2, contact.email3]))
 
 
 def clean(cont):
-    return Contact(id=cont.id, first_name=' '.join(cont.first_name.split()),
-                   last_name=' '.join(cont.last_name.split()))
+    first_name = ' '.join(cont.first_name.split())
+    last_name = ' '.join(cont.last_name.split())
+    address = clear_spaces_before_newline(cont.address)
+    all_phones_merged = merge_phones_like_on_home_page(cont)
+    all_emails_merged = clear_spaces_before_newline(merge_emails_like_on_home_page(cont))
+    # print(first_name, last_name, address, all_phones_merged, all_emails_merged)
+    return Contact(id=cont.id, first_name=first_name, last_name=last_name, address=address,
+                   all_phones_merged=all_phones_merged, all_emails_merged=all_emails_merged)
